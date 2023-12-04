2ad9780ee810ea81d5c37380b2cb5b80b568d4ba
Ian Kamar <ian.kamar@outlook.com>
2023-12-04 13:29:13 -0500

    update my project

--- task-manager/src/components/App/App.js
+++ task-manager/src/components/App/App.js
@@ -1,71 +1,115 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect, createContext, useContext } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { saveProfile } from "./redux/actions/authActions";
-import Register from "./Register";
-import Login from "./Login";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import { fetchTasks } from "../../utils/api";
 
 import "./App.css";
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const authState = useSelector((state) => state.authReducer);
-  const dispatch = useDispatch();
+// Define the AuthContext
+export const AuthContext = createContext();
+
+// Define the useAuth hook
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+// Define the AuthProvider component
+export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profile, setProfile] = useState(null);
+
+  const saveProfile = async (token) => {
+    try {
+      const response = await fetchTasks();
+
+      if (!response.ok) {
+        throw new Error("Profile fetch failed");
+      }
+
+      const profile = await response.json();
+      setProfile(profile);
+    } catch (error) {
+      console.error("Failed to fetch profile:", error);
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) return;
-    dispatch(saveProfile(token));
-  }, [authState.isLoggedIn, dispatch]);
+    setIsLoggedIn(true);
+    saveProfile(token);
+  }, []);
+
+  const value = {
+    isLoggedIn,
+    setIsLoggedIn,
+    profile,
+    saveProfile,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const { isLoggedIn } = useAuth();
 
   const addTask = (task) => {
     setTasks([...tasks, task]);
   };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/signup"
-          element={authState.isLoggedIn ? <Navigate to="/" /> : <Register />}
-        />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/tasks/add"
-          element={
-            authState.isLoggedIn ? (
-              <TaskInput addTask={addTask} />
-            ) : (
-              <Navigate to="/login" state={{ redirectUrl: "/tasks/add" }} />
-            )
-          }
-        />
-        <Route
-          path="/tasks/:taskId"
-          element={
-            authState.isLoggedIn ? (
-              <TaskList tasks={tasks} />
-            ) : (
-              <Navigate
-                to="/login"
-                state={{ redirectUrl: window.location.pathname }}
-              />
-            )
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/signup"
+            element={isLoggedIn ? <Navigate to="/" /> : <Register />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/tasks/add"
+            element={
+              isLoggedIn ? (
+                <TaskInput addTask={addTask} />
+              ) : (
+                <Navigate to="/login" state={{ redirectUrl: "/tasks/add" }} />
+              )
+            }
+          />
+          <Route
+            path="/tasks/:taskId"
+            element={
+              isLoggedIn ? (
+                <TaskList tasks={tasks} />
+              ) : (
+                <Navigate
+                  to="/login"
+                  state={{ redirectUrl: window.location.pathname }}
+                />
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
 function TaskInput({ addTask }) {
   const [input, setInput] = useState("");
+  const { isLoggedIn } = useAuth();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     addTask(input);
     setInput("");
   };
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" state={{ redirectUrl: "/tasks/add" }} />;
+  }
 
   return (
     <form onSubmit={handleSubmit} className="task-input">
@@ -82,6 +126,14 @@ function TaskInput({ addTask }) {
 }
 
 function TaskList({ tasks }) {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) {
+    return (
+      <Navigate to="/login" state={{ redirectUrl: window.location.pathname }} />
+    );
+  }
+
   return (
     <ul className="task-list">
       {tasks.map((task, index) => (
---
+++ task-manager/src/contexts/Authcontext.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, createContext, useContext } from "react";
+
+export const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const value = {
+    isLoggedIn,
+    setIsLoggedIn,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
