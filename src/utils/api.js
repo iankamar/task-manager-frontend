acf55ef9e475b8a86c30196b57ddf8bd8765cc71
Ian Kamar <ian.kamar@outlook.com>
2023-12-08 18:59:34 -0500

    update my project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
+/*
 import axios from "axios";
 
 const API_URL = "https://api.todoist.com/rest/v1";
@@ -30,3 +31,54 @@ export const updateTask = (taskId, updatedTaskData) =>
 export const deleteTask = (taskId) => api.delete(`/tasks/${taskId}`);
 
 export default api;
+*/
+
+// api.js
+export const apiConfig = {
+  baseUrl: "https://api.todoist.com/rest/v1",
+  headers: {
+    authorization: "Bearer 2b61d8158074902ceca67b61794de3fda171840d",
+    "Content-Type": "application/json",
+  },
+};
+
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this.baseUrl = baseUrl;
+    this.headers = headers;
+  }
+
+  _handleResponse(res) {
+    console.log(process.env.NODE_ENV, this.baseUrl);
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  }
+
+  getTaskList() {
+    return fetch(`${this.baseUrl}/tasks`, {
+      headers: this.headers,
+    }).then(this._handleResponse);
+  }
+
+  createTask(taskData) {
+    return fetch(`${this.baseUrl}/tasks`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(taskData),
+    }).then(this._handleResponse);
+  }
+
+  updateTask(taskId, updatedTaskData) {
+    return fetch(`${this.baseUrl}/tasks/${taskId}`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(updatedTaskData),
+    }).then(this._handleResponse);
+  }
+
+  deleteTask(taskId) {
+    return fetch(`${this.baseUrl}/tasks/${taskId}`, {
+      method: "DELETE",
+      headers: this.headers,
+    }).then(this._handleResponse);
+  }
+}
