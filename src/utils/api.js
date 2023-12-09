d0bd5e7ef5cd9d8f86d43ae2dce37e826b90475a
Ian Kamar <ian.kamar@Outlook.com>
2023-12-08 22:59:10 -0500

    update my project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,45 +1,8 @@
-/*
-import axios from "axios";
-
-const API_URL = "http://localhost:3001/todoist-proxy";
-//const API_URL = "https://api.todoist.com/rest/v1";
-const TODOIST_API_TOKEN = "2b61d8158074902ceca67b61794de3fda171840d";
-
-export const handleServerResponse = (res) => {
-  console.log(process.env.NODE_ENV, API_URL);
-  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-};
-
-export const request = (url, options) => {
-  return fetch(url, options).then(handleServerResponse);
-};
-
-const headers = {
-  Authorization: `Bearer ${TODOIST_API_TOKEN}`,
-  "Content-Type": "application/json",
-};
-
-const api = axios.create({
-  baseURL: API_URL,
-  headers,
-  withCredentials: true,
-});
-
-export const getTaskList = () => api.get("/tasks");
-export const createTask = (taskData) => api.post("/tasks", taskData);
-export const updateTask = (taskId, updatedTaskData) =>
-  api.post(`/tasks/${taskId}`, updatedTaskData);
-export const deleteTask = (taskId) => api.delete(`/tasks/${taskId}`);
-
-export default api;
-*/
-
 // api.js
 export const apiConfig = {
-  // baseUrl: "https://iankamar.github.io/task-manager-frontend/",
   baseUrl: "https://api.todoist.com/rest/v2",
   headers: {
-    authorization: "Bearer 2b61d8158074902ceca67b61794de3fda171840d",
+    authorization: "9d038fa32583784d3857e4bf3c43abae419d005c",
     "Content-Type": "application/json",
   },
 };
