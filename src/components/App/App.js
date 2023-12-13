a753b1116453d3c13d29bb50fcdf45c813b9cb5c
Ian Kamar <ian.kamar@outlook.com>
2023-12-13 10:24:11 -0500

    update my project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -155,11 +155,11 @@ const NavigationComponent = ({ tasks, setTasks }) => {
         />
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route
-          path="/tasks"
+          path="/task-manager-frontend/tasks"
           element={<TaskList tasks={tasks} onTaskClick={handleSelectedTask} />}
         />
         <Route
-          path="/create-task"
+          path="/task-manager-frontend/create-task"
           element={
             <CreateTask
               onCloseModal={handleCloseModal}
@@ -169,7 +169,7 @@ const NavigationComponent = ({ tasks, setTasks }) => {
           }
         />
         <Route
-          path="/update-task/:taskId"
+          path="/task-manager-frontend/update-task/:taskId"
           element={
             <UpdateTask
               task={selectedTask}
@@ -180,7 +180,7 @@ const NavigationComponent = ({ tasks, setTasks }) => {
           }
         />
         <Route
-          path="/delete-task/:taskId"
+          path="/task-manager-frontend/delete-task/:taskId"
           element={
             <DeleteTask
               task={selectedTask}
@@ -190,7 +190,7 @@ const NavigationComponent = ({ tasks, setTasks }) => {
             />
           }
         />
-        <Route path="/" element={<Main />} />
+        <Route path="/task-manager-frontend" element={<Main />} />
       </Routes>
 
       {activeModal === "preview" && (
