713fdfc2d1e4ab3444cd6aa9f2e0e3e536fe4686
Ian Kamar <ian.kamar@outlook.com>
2023-12-12 21:34:25 -0500

    update my project

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "auto",
+    publicPath: "/task-manager-frontend/",
   },
   resolve: {
     fallback: {
