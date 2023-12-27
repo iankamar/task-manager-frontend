c01b50c465224c216aeee8d11a8a3a51be554f29
Ian Kamar <ian.kamar@outlook.com>
2023-12-27 17:31:27 -0500

    update my project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,9 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import "./Header.css";
 
 const Header = ({
   isLoggedIn,
-  onCreateModal,
   openLoginModal,
   openRegisterModal,
   handleLogout,
@@ -15,47 +13,22 @@ const Header = ({
   return (
     <header className="header">
       <div className="header__avatar-logo">
-        <div>
-          {isLoggedIn ? (
-            <>
-              <button className="header__button me-3" onClick={handleLogout}>
-                Logout
-              </button>
-              <button
-                type="text"
-                onClick={onCreateModal}
-                className="header__item"
-              >
-                {" "}
-                + Add Task
-              </button>
-            </>
-          ) : (
+        {isLoggedIn ? (
+          <>
+            <h2 className="header__username">{currentUser?.name}</h2>
+            <button className="header__button me-3" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
             <button className="header__button" onClick={openRegisterModal}>
               Register
             </button>
-          )}
-        </div>
-        <div></div>
-        {isLoggedIn ? (
-          <Link to="/profile" className="header__profileLink">
-            {currentUser?.avatar === "" ? (
-              <div className="header__avatarPlaceholder">
-                {currentUser?.name[0]}
-              </div>
-            ) : (
-              <img
-                className="header__avatarIcon"
-                src={currentUser?.avatar}
-                alt={currentUser?.name}
-              />
-            )}
-            <h2 className="header__username">{currentUser?.name}</h2>
-          </Link>
-        ) : (
-          <button className="header__button" onClick={openLoginModal}>
-            Log in
-          </button>
+            <button className="header__button" onClick={openLoginModal}>
+              Log in
+            </button>
+          </>
         )}
       </div>
     </header>
