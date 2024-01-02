ff36df172cfe24c71d9df936ffee8bae3382e353
Ian Kamar <ian.kamar@outlook.com>
2024-01-02 15:05:00 -0500

    delete

--- src/components/PrivateRoute/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-// PrivateRoute.js
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children, isLoggedIn, ...props }) => {
-  return isLoggedIn ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
