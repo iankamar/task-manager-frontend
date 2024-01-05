7a7715b63602fd036b7c34b79b7d6c136a923074
Ian Kamar <ian.kamar@outlook.com>
2024-01-05 04:11:49 -0500

    update my project

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./home.css";
 
 const Home = () => {
@@ -5,6 +6,15 @@ const Home = () => {
     <div className="home-view">
       <h1>Welcome to Task Management</h1>
       <p>Your one-stop solution for managing your tasks efficiently.</p>
+      <div className="about">
+        Crafted with care, this task management tool showcases the author's
+        dedication to delivering user-friendly and efficient software solutions.
+      </div>
+      <div className="about">
+        The author's attention to detail ensures that your task management
+        process is both smooth and effective.
+      </div>
+      <Link to="/getTask">Go to Tasks</Link>
     </div>
   );
 };
