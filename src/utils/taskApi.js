6f0b5b2a59e431a4dd70d4f257f9c1490cb42f81
Ian Kamar <ian.kamar@outlook.com>
2024-01-09 11:17:21 -0500

    update my project

--- src/utils/taskApi.js
+++ src/utils/taskApi.js
@@ -1,7 +1,7 @@
 import { request } from "./api";
 
-//const API_URL = "https://api-iankamar-taskmanager.azurewebsites.net/api";
-const API_URL = "http://localhost:3001/api";
+const API_URL = "https://api-iankamar-taskmanager.azurewebsites.net/api";
+//const API_URL = "http://localhost:3001/api";
 
 export const getTaskList = async () => {
   const token = localStorage.getItem("token");
